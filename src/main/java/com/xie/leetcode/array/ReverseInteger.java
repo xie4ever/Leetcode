effc50dc9015761654ed6c9b3d512edc3697daef
xiehongtao <xie4ever@qq.com>
2018-11-12 23:41:32 +0800

    [xiehongtao] #整数反转问题 粗糙版本

---
+++ src/main/java/com/xie/leetcode/array/ReverseInteger.java
@@ -0,0 +1,48 @@
+package com.xie.leetcode.array;
+
+import java.util.Stack;
+
+/**
+ * @author xie4ever
+ * @date 2018/11/12 18:41
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        try {
+            long maxVal = 1;
+            maxVal = maxVal << 31;
+            if (x >= maxVal || x < (-1 * maxVal)) {
+                return 0;
+            }
+
+            String tempString = String.valueOf(x);
+            StringBuilder stringBuilder = new StringBuilder();
+            int s = 0;
+            if (String.valueOf(tempString.charAt(0)).equals("-")) {
+                stringBuilder.append(tempString.charAt(0));
+                s = 1;
+            }
+            Stack<String> stack = new Stack<String>();
+            for (int i = s; i < tempString.length(); i++) {
+                stack.push(String.valueOf(tempString.charAt(i)));
+            }
+            boolean flag = false;
+            while (!stack.empty()) {
+                if (!flag && stack.lastElement().equals("0")) {
+                    stack.pop();
+                    continue;
+                }
+                flag = true;
+                stringBuilder.append(stack.pop());
+            }
+            return Integer.valueOf(stringBuilder.toString());
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseInteger reverseInteger = new ReverseInteger();
+        System.out.println(reverseInteger.reverse(-2145500));
+    }
+}
